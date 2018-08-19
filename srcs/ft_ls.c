ed97cb9ad298e221ab1657ceef838bd1e93edad8
yatty <shin.ya.myad11@gmail.com>
2018-08-18 23:41:15 -0700

    solved memory leaks

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -28,26 +28,46 @@ static void		process_input(char **v, int opts, int argc)
 				j == 0 ? ft_printf("%s:\n", v[j]) : ft_printf("\n%s:\n", v[j]);
 			openread_dir(v[j], opts);
 		}
-		ft_strdel(&v[j++]);
+		j++;
 	}
 }
 
 int				main(int argc, char **argv)
 {
+	DIR				*dir;
 	char			**input;
+	char			*str;
 	int				opts;
 	int				j;
 
 	j = 0;
 	argv = check_option(&argc, argv, &opts);
 	if (argc == 1)
-		openread_dir(ft_strdup("."), opts);
+	{
+		str = ft_strdup(".");
+		openread_dir(str, opts);
+		ft_strdel(&str);
+		return (0);
+	}
 	input = (char **)malloc(sizeof(char *) * argc);
 	while (*++argv)
 		input[j++] = ft_strdup(*argv);
 	input[j] = 0;
 	quick_sort(&input, 0, argc - 2, !MATCH(opts, LR));
-	process_input(input, opts, argc);
+	//process_input(input, opts, argc);
+	j = 0;
+	while (input[j])
+	{
+		if (!(dir = opendir(input[j])))
+			get_file(input[j], opts);
+		else
+		{
+			if (argc > 2)
+				j == 0 ? ft_printf("%s:\n", input[j]) : ft_printf("\n%s:\n", input[j]);
+			openread_dir(input[j], opts);
+		}
+		j++;
+	}
 	delete_input(input);
 	while (1);
 	return (0);
